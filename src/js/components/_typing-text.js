7d8e5c71888f4843c03963cdf8d637af1521367a
Sav1oroff <saviorov@gmail.com>
2022-03-03 22:35:12 +0300

    Initial commit

---
+++ src/js/components/_typing-text.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing-text', {
+    strings : ['front end developer', 'web designer', 'graphic designer', 'web developer'],
+    loop : true,
+    typeSpeed : 150
+});
+
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+import './components/_burger-menu';
+import './components/_typing-text';
+import './components/_particle';
+
+
+
+
